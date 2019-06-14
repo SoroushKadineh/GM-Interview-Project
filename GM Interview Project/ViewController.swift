18023f798ef457b3cdb813b90ca2bc9a760f55d4
SoroushKadineh <soroushkadineh@gmail.com>
2019-06-13 20:19:56 -0500

    Initial Commit

---
+++ GM Interview Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GM Interview Project
+//
+//  Created by Soroush Kadineh on 6/13/19.
+//  Copyright © 2019 Soroush Kadineh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
